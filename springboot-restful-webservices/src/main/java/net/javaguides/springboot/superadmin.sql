702fdf1675353d8764f81599140c3af487b45e70
mokshit03 <mokshit.bhandari03@gmail.com>
2025-04-13 16:34:34 +0530

    update

--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/superadmin.sql
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/superadmin.sql
@@ -4,7 +4,7 @@ WHERE NOT EXISTS (SELECT 1 FROM roles WHERE role_name = 'Service_Admin');
  
 INSERT INTO users (user_id, username, email, firstname, lastname, status, version, password)
 SELECT 1,'serviceadmin', 'sadmin@gmail.com', 'Service', 'Admin', 'active', 1, '$2a$12$K7UMOmGfVHX7MSKN8NAN8eleFZBHdOKYaDP1RhBQeSFf./V3d0j02'
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'superadmin');
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'serviceadmin');
  
 INSERT INTO user_roles (user_id, role_id)
 SELECT 1, 1
